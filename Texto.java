2d45a1dc64e7f62972249210a359a0d115d38875
Leonardo Holtz <leo.holtz98@gmail.com>
2019-06-25 17:08:33 -0300

    parser terminado
    Chamadas das funções da classe texto estão em MyMusicApp e foram feitos 2 testes em JUnit4 que estão funcionando corretamente.

---
+++ Texto.java
@@ -0,0 +1,185 @@
+import java.lang.Character;
+
+public class Texto {
+	
+	private static String oitava;
+	private static int oitavaInt;
+	private static int instrumento;
+	private static String instrumentoString;
+	private static int volume;
+	private static String volumeString;
+	private static final String OITAVA_DEFAULT = "5";
+	private static final int INSTRUMENTO_DEFAULT = 0;
+	private static final int VOLUME_DEFAULT = 127;
+	public static String stringMusical;
+	
+	public static boolean charValido(char caractere) {
+		// True se o caractere altera algo na música, false caso o contrário
+		switch (caractere) {
+			case 'A':
+			case 'B':
+			case 'C':
+			case 'D':
+			case 'E':
+			case 'F':
+			case 'G':
+			case ' ':
+			case 'I':
+			case 'i':
+			case 'O':
+			case 'o':
+			case 'U':
+			case 'u':
+			case '?': // Aumenta oitava
+			case 'Q': // Diminui oitava
+			case '.':
+			case '!':	
+			case '\n':
+			case ';':
+			case ',':
+			case 'X': // Pausa
+				return true;
+			default:
+				return false;
+		}
+	}
+	
+	private static boolean ehNota(char caractere) {
+		// True se o char é uma nota válida, false caso o contrário
+		switch (caractere) {
+			case 'A':
+			case 'B':
+			case 'C':
+			case 'D':
+			case 'E':
+			case 'F':
+			case 'G':
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	public String formataTexto(String inputTexto) {
+		char[] textoFormatado = inputTexto.toCharArray();
+		for(int i = 0; i < textoFormatado.length; i++) {
+			if(i == 0) {
+				if(!charValido(textoFormatado[i]))
+					textoFormatado[i] = 'X';	// O caractere X indicará uma pausa
+			}
+			else {
+				if(!charValido(textoFormatado[i])) {
+					if(ehNota(textoFormatado[i-1]))
+						textoFormatado[i] = textoFormatado[i-1];
+					else
+						textoFormatado[i] = 'X';
+				}
+			}
+		}
+		String textoFormatadoFinal = new String(textoFormatado);
+		return textoFormatadoFinal;
+	}
+	
+	private static boolean ehOutraVogal(char c) {
+		switch (c) {
+			case 'I':
+			case 'i':
+			case 'O':
+			case 'o':
+			case 'U':
+			case 'u':
+				return true;
+			default:
+				return false;
+		}
+	}
+	
+	
+	public String textoParaMusicString(String textoFormatado) {
+		char[] textoFormatadoChar = textoFormatado.toCharArray();
+		StringBuilder arrayMusical = new StringBuilder();
+		oitava = OITAVA_DEFAULT;
+		instrumento = INSTRUMENTO_DEFAULT;
+		volume = VOLUME_DEFAULT;
+		for(int i = 0; i < textoFormatadoChar.length; i++) {
+			if(ehNota(textoFormatadoChar[i])) {
+				// Padrão estipulado: Oitavas sempre serão concatenadas com as notas musicais
+				arrayMusical.append(textoFormatadoChar[i]);
+				arrayMusical.append(oitava + " ");
+			}
+			if(textoFormatadoChar[i] == 'X') {
+				arrayMusical.append('R'); // Rest (pausa)
+				arrayMusical.append(' ');
+			}
+			if(textoFormatadoChar[i] == '!') { // Muda instrumento para Harpsichord
+				instrumento = 6;
+				instrumentoString = Integer.toString(instrumento);
+				arrayMusical.append("I" + instrumentoString);
+				arrayMusical.append(' ');
+			}
+			if(textoFormatadoChar[i] == ';') { // Muda instrumento para Pan Flute
+				instrumento = 75;
+				instrumentoString = Integer.toString(instrumento);
+				arrayMusical.append("I" + instrumentoString);
+				arrayMusical.append(' ');
+			}
+			if(textoFormatadoChar[i] == '\n') { // Muda instrumento para Tubular Bells
+				instrumento = 14;
+				instrumentoString = Integer.toString(instrumento);
+				arrayMusical.append("I" + instrumentoString);
+				arrayMusical.append(' ');
+			}
+			if(textoFormatadoChar[i] == ',') { // Muda instrumento para Tubular Bells
+				instrumento = 19;
+				instrumentoString = Integer.toString(instrumento);
+				arrayMusical.append("I" + instrumentoString);
+				arrayMusical.append(' ');
+			}
+			if (Character.isDigit(textoFormatadoChar[i])) {
+				if(instrumento == 127) // Se esta no último instrumento, volta para o piano
+					instrumento = Character.getNumericValue(textoFormatadoChar[i]) - 1;
+				else					// Se não, vai para o próximo.
+					instrumento = instrumento + Character.getNumericValue(textoFormatadoChar[i]);
+			}
+			if(textoFormatadoChar[i] == '?') { // Aumeta a oitava em 1
+				if(oitava == "9")
+					oitava = OITAVA_DEFAULT;
+				else {							//ARRUMAR ESSA PORRA
+					oitavaInt = Integer.parseInt(oitava);
+					oitavaInt++;
+					oitava = Integer.toString(oitavaInt);
+				}
+			}
+			if(textoFormatadoChar[i] == 'Q') { // Diminui a oitava em 1
+				if(oitava == "1")
+					oitava = OITAVA_DEFAULT;
+				else {							//ARRUMAR ESSA PORRA
+					oitavaInt = Integer.parseInt(oitava);
+					oitavaInt--;
+					oitava = Integer.toString(oitavaInt);
+				}
+			}
+			if(textoFormatadoChar[i] == ' ') {
+				if(volume * 2 >= 127)
+					volume = 127;
+				else
+					volume = volume * 2;
+				volumeString = ":CON(7, " + Integer.toString(volume) + ") ";
+				arrayMusical.append(volumeString);
+			}
+			if(ehOutraVogal(textoFormatadoChar[i])) {
+				if(volume + 12 >= 127)
+					volume = 127;
+				else
+					volume = volume + 12;
+				volumeString = ":CON(7, " + Integer.toString(volume) + ") ";
+				arrayMusical.append(volumeString);
+			}
+		}
+		stringMusical = arrayMusical.toString();
+		return stringMusical;
+		
+	}
+
+
+}
