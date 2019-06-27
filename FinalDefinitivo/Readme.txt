9946935c862210ccbd72b8d3cd0d9205b83201f8
LeonardoHoltz <leo.holtz98@gmail.com>
2019-06-27 02:45:18 -0300

    Fim do trabalho
    A pasta Final Definitivo possui todos os arquivos.

---
+++ FinalDefinitivo/Readme.txt
@@ -0,0 +1,2 @@
+O .Jar executável está na pasta gerasom/dist/gerasom.jar
+Ele necessita estar no mesmo diretório onde a pasta lib está.
--- VersaoPronta/src/gerasom/Musica.java
+++ VersaoPronta/src/gerasom/Musica.java
@@ -1,13 +1,5 @@
 package gerasom;
 
-
-/* Megalovania:
-	/* "D4i D4i D5q A4qi Ab4i Ri G4qi F4q D4i F4i G4i "
-					+ "C4i C4i D5q A4qi Ab4i Ri G4qi F4q D4i F4i G4i "
-					+ "B3i B3i D5q A4qi Ab4i Ri G4qi F4q D4i F4i G4i "
-					+ "Bb3i Bb3i D5q A4qi Ab4i Ri G4qi F4q D4i F4i G4i " */
- /* Sugest�o pra Teste: Alguma musica de Ocarina do Zelda */
- /* :CON(7, y) controla o volume, sendo o menor 1 e o maior 127 */
 import java.io.File;
 import java.io.IOException;
 
@@ -16,9 +8,9 @@ import org.jfugue.Player;
 
 public class Musica {
 
-    public static String entradaUsuario; // = " QBX?DQAXGABX?DQA";	// fornecida pelo usu�rio
-    public static String tempoBPM; // = "Vivace"; // selecionado pelo usu�rio
-    public static String nomeArquivo; // = "jesus"; // fornecido pelo usu�rio
+    public static String entradaUsuario;
+    public static String tempoBPM;
+    public static String nomeArquivo;
     public static Player player = new Player();
 
     public static void setEntradaUsuario(String entradaUsuario) {
@@ -51,16 +43,6 @@ public class Musica {
 
     public static Player getPlayer() {
         return player;
-    }
-
-    public static void main(String[] args) throws IOException {
-//        Texto musica = new Texto();
-//        stringIntermediaria = musica.formataTexto(entradaUsuario);
-//        stringFinal = musica.textoParaMusicString(stringIntermediaria, tempoBPM);
-//        Pattern pattern = new Pattern(stringFinal);
-
-//        tocaMusica(pattern);
-        // criacaoMidi(pattern, nomeArquivo);
     }
 
     public static Pattern textToPattern(String entradaUsuario, String tempoBPM) {
@@ -96,4 +78,4 @@ public class Musica {
     }
 
 }
-/* Zelda Lullaby: " QBX?DQAXGABX?DQA" = ":CON(7, 127) B4 R D5 A4 R G4 A4 B4 R D5 A4 R R" */
+
